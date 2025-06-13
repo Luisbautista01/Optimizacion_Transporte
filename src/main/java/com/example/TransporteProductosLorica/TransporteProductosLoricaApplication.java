b81b30c6c9e50829f6f064e3d2574ba3be6d1eae
Luisbautista01 <bautistaluisantonio24@gmail.com>
2025-06-12 22:21:07 -0500

    [feature][task 1][1]

---
+++ src/main/java/com/example/TransporteProductosLorica/TransporteProductosLoricaApplication.java
@@ -0,0 +1,13 @@
+package com.example.TransporteProductosLorica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransporteProductosLoricaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransporteProductosLoricaApplication.class, args);
+	}
+
+}
